1b2ba9e813f3aab32993ff39908ccfcfa576f077
Ashirot <juliopajota9@gmail.com>
2024-08-22 11:35:57 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+#firstNum = input("Enter First Number: ")
+#secNum = input("Enter Second NUmber: ")
+#if firstNum < secNum:
+ #   print (f"{firstNum} is less than {secNum}")
+#elif firstNum > secNum:
+ #   print(f"{firstNum} is greater than {secNum}")
+#elif firstNum == secNum:
+ #   print(f"{firstNum} is equals to {secNum}")
+
+firstname = input("Enter Classmate name 1 : ")
+secondname = input("Enter Classmatae name 2: ")
+thirdname = input ("Enter Classnate name 3: ")
+print(f"Your classmates are: {firstname} {secondname} {thirdname}")
